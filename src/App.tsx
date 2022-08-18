30e6288e61893a856bb726c403fa87d75d374223
allan victor <allanviictor07@gmail.com>
2022-08-18 20:56:51 -0300

    arquivos iniciais projeto list repo com vite e joao victor

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from './components/header/header'
+
+function App() {
+
+  return(
+    <>
+      <Header/>
+      
+      <h1 className="text-green-600 text-3xl font-bold underline">allan</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header() {
+    return(
+        <header className="w-full bg-slate-900 py-5">
+            <h3 className="text-white text-center text-2xl">Listando Repositorios</h3> 
+        </header>
+    )
+}
